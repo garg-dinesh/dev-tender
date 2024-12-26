8cdad8ca92eed8e881f8629726b1a47cbf3513d5
garg-dinesh <garg.dinesh92@gmail.com>
2024-12-27 12:29:31 +1300

    Routing handling

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,21 +2,35 @@ const express = require('express')
 
 const app = express();
 
-// This will only handle GET call to /user
-app.get('/user', (req, res) => {
+// b => optional
+// app.get('/ab?c', (req, res) => {
+//     res.send({firstname: 'Dinesh', lastname: 'Garg'})
+// })
+
+// b => more than one times
+// app.get('/ab+c', (req, res) => {
+//     res.send({firstname: 'Dinesh', lastname: 'Garg'})
+// })
+
+// * => can be any thing
+// app.get('/a*c', (req, res) => {
+//     res.send({firstname: 'Dinesh', lastname: 'Garg'})
+// })
+
+// app.get('/user', (req, res) => {
+//     console.log('Query: ', req.query)
+//     res.send({firstname: 'Dinesh', lastname: 'Garg'})
+// })
+
+// app.get('/user/:userId', (req, res) => {
+//     console.log('Params: ', req.params)
+//     res.send({firstname: 'Dinesh', lastname: 'Garg'})
+// })
+
+app.get('/user/:userId/:name', (req, res) => {
+    console.log('Params: ', req.params)
     res.send({firstname: 'Dinesh', lastname: 'Garg'})
 })
-
-app.post('/user', (req, res) => {
-    console.log('Save Data in database..')
-    res.send('Save Data in database..')
-})
-
-// this will match all the cmd
-// http methods API call to /test
-// app.use('/test', (req, res) => {
-//     res.send('This is from test url...')
-// })
 
 app.listen(3000, () => {
     console.log('Server is successfully listening on the port 3000....')
