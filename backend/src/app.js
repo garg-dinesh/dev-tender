b9852a89661f1442d5515550c13b5a539ca88b09
garg-dinesh <garg.dinesh92@gmail.com>
2024-12-27 14:35:08 +1300

    multiple route handler'

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,35 +2,19 @@ const express = require('express')
 
 const app = express();
 
-// b => optional
-// app.get('/ab?c', (req, res) => {
-//     res.send({firstname: 'Dinesh', lastname: 'Garg'})
-// })
-
-// b => more than one times
-// app.get('/ab+c', (req, res) => {
-//     res.send({firstname: 'Dinesh', lastname: 'Garg'})
-// })
-
-// * => can be any thing
-// app.get('/a*c', (req, res) => {
-//     res.send({firstname: 'Dinesh', lastname: 'Garg'})
-// })
-
-// app.get('/user', (req, res) => {
-//     console.log('Query: ', req.query)
-//     res.send({firstname: 'Dinesh', lastname: 'Garg'})
-// })
-
-// app.get('/user/:userId', (req, res) => {
-//     console.log('Params: ', req.params)
-//     res.send({firstname: 'Dinesh', lastname: 'Garg'})
-// })
-
-app.get('/user/:userId/:name', (req, res) => {
-    console.log('Params: ', req.params)
-    res.send({firstname: 'Dinesh', lastname: 'Garg'})
-})
+app.use('/user',
+    (req, res, next) => {
+        // Route handler
+        console.log('Handling the route user')
+        // res.send('Response !!')
+        next()
+``  },
+    (req, res) => {
+        // Route handler 2
+        console.log('Handling the route user')
+        res.send('Response2 !!')
+    }
+)
 
 app.listen(3000, () => {
     console.log('Server is successfully listening on the port 3000....')
