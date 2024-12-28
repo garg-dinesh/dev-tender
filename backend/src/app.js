e72f7365548e2ab584b1390f15d4b44cf3ec0ea3
garg-dinesh <garg.dinesh92@gmail.com>
2024-12-28 17:04:23 +1300

    Get APIs

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,14 +5,41 @@ const User = require('./models/user');
 
 const app = express();
 
+app.use(express.json ())
+
+app.get('/feed', async (req, res) => {
+    try {
+        const users = await User.find({});
+        return res.send(users)
+    } catch(err) {
+        return res.status(400).send('Something went wrong..')
+    }
+})
+
+app.get('/user', async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        // const users = await User.find({emailId: userEmail});
+
+        // if(users.length === 0) {
+        //     return res.status(404).send('User not found..')
+        // }
+        // return res.send(users)
+
+        const user = await User.findOne({emailId: userEmail});
+
+        if (!user) {
+            return res.status(404).send('User not found..')
+        }
+        return res.send(user)
+    } catch(err) {
+        return res.status(400).send('Something went wrong..')
+    }
+})
+
 app.post('/signup', async (req, res) => {
     // Creating new instance of User model
-    const user = new User({
-        firstName: 'Dinesh',
-        lastName: 'Garg',
-        emailId: 'dinesh@garg.com',
-        password: 'dinesh@123'
-    });
+    const user = new User(req.body);
 
     try {
         await user.save();
